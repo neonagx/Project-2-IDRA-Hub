73731ce5d7153f762c4bcd7cd6769c0333dec453
Sukhyun Baik <neonagx@gmail.com>
2016-10-28 15:46:52 -0700

    added form for for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028220354) do
+ActiveRecord::Schema.define(version: 20161028224208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,13 +19,13 @@ ActiveRecord::Schema.define(version: 20161028220354) do
     t.string   "username"
     t.string   "name"
     t.string   "password_digest"
-    t.string   "date_of_birth"
     t.boolean  "gender"
     t.boolean  "admin"
     t.string   "phone"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.date     "date_of_birth"
   end
 
 end
