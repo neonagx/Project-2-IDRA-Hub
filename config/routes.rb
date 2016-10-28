1490135bbcf409f1b5ca13a0f0452de91b12c932
Sukhyun Baik <neonagx@gmail.com>
2016-10-28 16:39:52 -0700

    added controller for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root '/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users' => 'users#index'
   get 'users/new' => 'users#new', as: :new_user
