f6224c929767c6703230078cf003197a216d49b8
Sukhyun Baik <neonagx@gmail.com>
2016-10-31 09:42:08 -0700

    adds controller and edits index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,40 @@
 class ProductsController < ApplicationController
+  before_action :set_garment, only: [:show, :edit, :update, :destroy]
   def index
+    @products = Product.all
   end
 
   def show
   end
 
   def new
+    @product = Product.new
   end
 
   def edit
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :for_sale)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
