8a11ff7cda73fc814c1e272f391fd9302f0f93b5
Sukhyun Baik <neonagx@gmail.com>
2016-11-01 11:11:25 -0700

    adds comment model and migrates the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031230840) do
+ActiveRecord::Schema.define(version: 20161101180451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "user_id"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -39,5 +49,7 @@ ActiveRecord::Schema.define(version: 20161031230840) do
     t.date     "date_of_birth"
   end
 
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
   add_foreign_key "products", "users"
 end
