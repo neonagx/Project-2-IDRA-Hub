4945bce6cbbb741b87b51a84f6c643692e7d3a77
Sukhyun Baik <neonagx@gmail.com>
2016-11-02 11:21:11 -0700

    adds sizing to model product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,9 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_attached_file :image, styles: {
-    small: '300x300',
-    med: '600x600',
-    large: '1000x1000'
+    small: '200x200',
+    med: '500x500',
+    large: '800x800'
   }
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
