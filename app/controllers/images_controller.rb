3f3215941e513f7eb999cc219cb81d65439cb2af
Sukhyun Baik <neonagx@gmail.com>
2016-11-03 15:59:33 -0700

    adds more images with new html

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,24 @@
 class ImagesController < ApplicationController
   def new
+    @product = Product.find(params[:product_id])
     @image = Image.new
+  end
+
+  def create
+    @user = User.find(session[:user_id])
+    @product = Product.find(params[:product_id])
+    @image = @product.images.new(params.require(:image).permit(:image))
+    if @image.save
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @product = Product.find(params[:product_id])
+    @image.destroy
+    redirect_to @product
   end
 end
